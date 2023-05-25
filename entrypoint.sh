20bd0fca87e78f37dc9c43764e0fc33ae6189d17
Denis Volk <denis.volk@gmail.com>
2023-05-25 06:42:11 +0000

    add guards in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
-flask db init
-flask db migrate
+
+# Check if the INIT_DB variable is set
+if [ "$INIT_DB" = "true" ]
+then
+    flask db init
+fi
+
+# Check if the MIGRATE_DB variable is set
+if [ "$MIGRATE_DB" = "true" ]
+then
+    flask db migrate
+fi
+
 flask db upgrade
+
 exec python3 app.py "$@"
