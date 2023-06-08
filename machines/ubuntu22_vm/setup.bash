317da02e408b2e332c4d5fc0bb7b1099f189125f
Denis Volk <denis.volk@gmail.com>
2023-06-08 21:02:48 +0000

    install deb firefox over snap firefox on ubuntu 22.04

--- machines/ubuntu22_vm/setup.bash
+++ machines/ubuntu22_vm/setup.bash
@@ -15,6 +15,8 @@ apt-get install -y dbus-x11 xfce4 xfce4-goodies xfonts-base xfonts-100dpi \
     tigervnc-xorg-extension novnc websockify nginx nginx-extras sudo curl \
     unzip scrot cron
 
+
+
 # Download and install filebrowser
 curl -fsSL $FILEBROWSER_DL -o filebrowser.tar.gz
 tar -xzf filebrowser.tar.gz
@@ -22,12 +24,18 @@ mv filebrowser /usr/local/bin
 chmod +x /usr/local/bin/filebrowser
 rm -f filebrowser.tar.gz
 
+
+
 # Install some more useful utilities
 apt-get install -y coreutils findutils grep sed gawk gzip tar curl wget git openssl \
     vim nano tmux htop ncdu tree file less bc zip unzip ssh rsync procps screenfetch
 
+
+
 # Install desktop applications
-apt-get -y install firefox-esr webext-ublock-origin-firefox emacs
+apt-get -y install emacs
+
+
 
 # Add desktop user 'ubuntu'
 useradd -m -s /bin/bash $USER
@@ -35,6 +43,8 @@ echo "$USER:$USER" | chpasswd
 echo "$USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$USER
 chmod 0440 /etc/sudoers.d/$USER
 usermod -aG audio,video,cdrom,plugdev,staff,adm,dialout,sudo $USER
+
+
 
 # Set up VNC
 mkdir -p /home/$USER/.vnc
@@ -44,9 +54,13 @@ chmod 600 /home/$USER/.vnc/passwd
 apt purge -y xfce4-power-manager
 rm /etc/nginx/sites-enabled/default
 
+
+
 # Allow the user to save screenshots to web server
 mkdir /var/www/html/screenshots
 chown -R $USER:$USER /var/www/html/screenshots
+
+
 
 # Copy the nginx config file
 cp nginx-ada.conf /etc/nginx/sites-enabled/nginx-ada.conf
@@ -54,31 +68,67 @@ cp nginx-ada.conf /etc/nginx/sites-enabled/nginx-ada.conf
 cp index.html /var/www/html/index.html
 cp ada.png /var/www/html/ada.png
 
+
+
 # cronjob: take a screenshot every minute
 crontab -l -u $USER | { cat; echo '* * * * * DISPLAY=:0 scrot -z -t 20 -o /var/www/html/screenshots/screenshot.png'; } | crontab -u $USER -
 
+
+
 # don't boot into X by default since it's started by vncserver
 sudo systemctl set-default multi-user.target
+
+
 
 # copy systemd unit files
 cp filebrowser.service /etc/systemd/system
 cp websockify.service /etc/systemd/system
 cp vncserver.service /etc/systemd/system
 
+
+
 # reload systemd
 systemctl daemon-reload
+
+
 
 # enable ada services
 systemctl enable vncserver.service filebrowser.service websockify.service
 systemctl start vncserver.service filebrowser.service websockify.service
 
+
+
+# remove snap firefox and install deb version
+
+# snap firefox doesn't seem to work with the way the desktop
+# is started with vncserver. This seems to be a common
+# problem with no obvious solution, but it might be solved
+# in future ubuntu releases
+snap remove firefox
+
+add-apt-repository -y ppa:mozillateam/ppa
+
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | tee /etc/apt/preferences.d/mozilla-firefox
+
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+
+apt install -y firefox webext-ublock-origin-firefox
+
+
+
 # OPTIONAL: install R and Rstudio server
 apt install -y r-base
-# TODO host this locally
+# TODO host this file locally
 wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2023.06.0-421-amd64.deb
 dpkg -i rstudio-server-2023.06.0-421-amd64.deb
 apt install -y -f
 rm rstudio-server-2023.06.0-421-amd64.deb
+
+
 
 # OPTIONAL: Install Python and jupyter notebook
 apt install -y python3-pip python3-venv
@@ -96,5 +146,7 @@ systemctl daemon-reload
 systemctl enable jupyter.service
 systemctl start jupyter.service
 systemctl restart nginx
+
+
 
 ### THE END
