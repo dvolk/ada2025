50f1b565c4b82b6a6bf009957df02432250c0b73
Denis Volk <denis.volk@gmail.com>
2023-06-08 20:18:42 +0000

    add ub 22.04 machine config w jupyter notebook and rstudio

---
+++ machines/ubuntu22_vm/setup.bash
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Set environment variables
+export DEBIAN_FRONTEND=noninteractive
+export VNC_PORT=5900
+export NOVNC_PORT=6080
+export VNC_PW=vncpassword
+export USER=ubuntu
+export FILEBROWSER_DL=https://github.com/filebrowser/filebrowser/releases/download/v2.23.0/linux-amd64-filebrowser.tar.gz
+
+# Update packages and install required packages
+apt-get update
+apt-get install -y dbus-x11 xfce4 xfce4-goodies xfonts-base xfonts-100dpi \
+    xfonts-75dpi xfonts-scalable tigervnc-standalone-server tigervnc-common \
+    tigervnc-xorg-extension novnc websockify nginx nginx-extras sudo curl \
+    unzip scrot cron
+
+# Download and install filebrowser
+curl -fsSL $FILEBROWSER_DL -o filebrowser.tar.gz
+tar -xzf filebrowser.tar.gz
+mv filebrowser /usr/local/bin
+chmod +x /usr/local/bin/filebrowser
+rm -f filebrowser.tar.gz
+
+# Install some more useful utilities
+apt-get install -y coreutils findutils grep sed gawk gzip tar curl wget git openssl \
+    vim nano tmux htop ncdu tree file less bc zip unzip ssh rsync procps screenfetch
+
+# Install desktop applications
+apt-get -y install firefox-esr webext-ublock-origin-firefox emacs
+
+# Add desktop user 'ubuntu'
+useradd -m -s /bin/bash $USER
+echo "$USER:$USER" | chpasswd
+echo "$USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$USER
+chmod 0440 /etc/sudoers.d/$USER
+usermod -aG audio,video,cdrom,plugdev,staff,adm,dialout,sudo $USER
+
+# Set up VNC
+mkdir -p /home/$USER/.vnc
+echo $VNC_PW | vncpasswd -f > /home/$USER/.vnc/passwd
+chown -R $USER:$USER /home/$USER/.vnc
+chmod 600 /home/$USER/.vnc/passwd
+apt purge -y xfce4-power-manager
+rm /etc/nginx/sites-enabled/default
+
+# Allow the user to save screenshots to web server
+mkdir /var/www/html/screenshots
+chown -R $USER:$USER /var/www/html/screenshots
+
+# Copy the nginx config file
+cp nginx-ada.conf /etc/nginx/sites-enabled/nginx-ada.conf
+# Copy nginx front page
+cp index.html /var/www/html/index.html
+cp ada.png /var/www/html/ada.png
+
+# cronjob: take a screenshot every minute
+crontab -l -u $USER | { cat; echo '* * * * * DISPLAY=:0 scrot -z -t 20 -o /var/www/html/screenshots/screenshot.png'; } | crontab -u $USER -
+
+# don't boot into X by default since it's started by vncserver
+sudo systemctl set-default multi-user.target
+
+# copy systemd unit files
+cp filebrowser.service /etc/systemd/system
+cp websockify.service /etc/systemd/system
+cp vncserver.service /etc/systemd/system
+
+# reload systemd
+systemctl daemon-reload
+
+# enable ada services
+systemctl enable vncserver.service filebrowser.service websockify.service
+systemctl start vncserver.service filebrowser.service websockify.service
+
+# OPTIONAL: install R and Rstudio server
+apt install -y r-base
+# TODO host this locally
+wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2023.06.0-421-amd64.deb
+dpkg -i rstudio-server-2023.06.0-421-amd64.deb
+apt install -y -f
+rm rstudio-server-2023.06.0-421-amd64.deb
+
+# OPTIONAL: Install Python and jupyter notebook
+apt install -y python3-pip python3-venv
+
+su ubuntu << EOF
+python3 -m venv /home/ubuntu/jupyter-env
+/home/ubuntu/jupyter-env/bin/pip3 install notebook
+mkdir -p /home/ubuntu/.jupyter
+mkdir -p /home/ubuntu/notebooks
+cp jupyter_notebook_config.py /home/ubuntu/.jupyter/jupyter_notebook_config.py
+EOF
+
+cp jupyter.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable jupyter.service
+systemctl start jupyter.service
+systemctl restart nginx
+
+### THE END
