8200d8c488fcdc578e8c62da75899bc11eefd391
Denis Volk <denis.volk@gmail.com>
2023-08-06 15:47:17 +0100

    update jupyter notebook config for ver 7

--- machines/ubuntu22/jupyter_notebook_config.py
+++ machines/ubuntu22/jupyter_notebook_config.py
@@ -1,17 +1,20 @@
 c = get_config()  # noqa
 
 # serve jupyter notebook on /jupyter-notebook/ not /
-c.NotebookApp.base_project_url = "/jupyter-notebook/"
+c.ServerApp.base_url = "/jupyter-notebook/"
 
 # set default dir
-c.NotebookApp.notebook_dir = "/home/ubuntu/notebooks"
+c.ServerApp.notebook_dir = "/home/ubuntu/notebooks"
 
 # we're using nginx to proxy to notebook
-c.NotebookApp.allow_remote_access = True
+c.ServerApp.allow_remote_access = True
 
 # we're running in systemd - don't open browser
-c.NotebookApp.open_browser = False
+c.ServerApp.open_browser = False
 
 # disable authentication as we're using an authentication proxy
-c.NotebookApp.password = ""
-c.NotebookApp.token = ""
+c.ServerApp.password = ""
+c.ServerApp.token = ""
+
+# prevent bug with xsrf cookie
+c.ServerApp.disable_check_xsrf = True
