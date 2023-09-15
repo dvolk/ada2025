fc5766d2f02dbab180415da7c0ca28e84e4cf4af
Denis Volk <denis.volk@gmail.com>
2023-09-15 13:44:04 +0100

    remove dash from group flavor

--- machines/ubuntu22/setup.bash
+++ machines/ubuntu22/setup.bash
@@ -561,7 +561,7 @@ xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-
 EOF
 fi
 
-if [ "$BUILD_GROUP_FLAVOR" = "generic training" ]; then
+if [ "$BUILD_GROUP_FLAVOR" = "generic-training" ]; then
     # - Change the theme to Adwaita
     # - Change the icons to Tango
     # - Turn off xfce panel dark mode
