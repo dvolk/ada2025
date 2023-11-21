84c4f76217dfc4780906e6ad03dda4931fd9789e
CallumLee22 <callum.lee@stfc.ac.uk>
2023-11-21 15:15:03 +0000

    return json format

--- quota_check.py
+++ quota_check.py
@@ -18,5 +18,68 @@ def main(machine_provider_id):
             "OS_PROJECT_DOMAIN_NAME": prov_data["project_domain_name"],
             "OS_USERNAME": prov_data["username"],
             "OS_PASSWORD": prov_data["password"],
-            "OS_PROJECT_NAME": prov_data["project_name"]
+            "OS_PROJECT_NAME": prov_data["project_name"],
         }
+
+        result = subprocess.run(
+            ["openstack", "server", "list", "-f", "json"],
+            capture_output=True,
+            env=env,
+        )
+
+        server_list = json.loads(result.stdout.decode())
+
+        result = subprocess.run(
+            ["openstack", "flavor", "list", "-f", "json"],
+            capture_output=True,
+            env=env,
+        )
+
+        flavor_list = json.loads(result.stdout.decode())
+
+        instance_count = 0
+        active_ram = 0
+        active_cpu = 0
+        shelved_ram = 0
+        shelved_cpu = 0
+        shut_down = []
+        for instance in server_list:
+            if server_list[instance_count]["Status"] == "ACTIVE":
+                flavor = server_list[instance_count]["Flavor"]
+                flavor_count = 0
+                for flavors in flavor_list:
+                    if (
+                        flavor_list[flavor_count]["Name"]
+                        == server_list[instance_count]["Flavor"]
+                    ):
+                        active_ram += flavor_list[flavor_count]["RAM"]
+                        active_cpu += flavor_list[flavor_count]["VCPUs"]
+                    flavor_count += 1
+            elif server_list[instance_count]["Status"] == "SHELVED_OFFLOADED":
+                flavor = server_list[instance_count]["Flavor"]
+                flavor_count = 0
+                for flavors in flavor_list:
+                    if (
+                        flavor_list[flavor_count]["Name"]
+                        == server_list[instance_count]["Flavor"]
+                    ):
+                        shelved_ram += flavor_list[flavor_count]["RAM"]
+                        shelved_cpu += flavor_list[flavor_count]["VCPUs"]
+                    flavor_count += 1
+            elif server_list[instance_count]["Status"] == "SHUTOFF":
+                shut_down.append(server_list[instance_count]["Name"])
+            instance_count += 1
+
+        total_ram = active_ram + shelved_ram
+        total_cpu = active_cpu + shelved_cpu
+
+        usage = {
+            "active_ram": active_ram,
+            "active_cpu": active_cpu,
+            "shelved_ram": shelved_ram,
+            "shelved_cpu": shelved_cpu,
+            "total_ram": total_ram,
+            "total_cpu": total_cpu
+        }
+
+        return json.dump(usage, indent=4)
